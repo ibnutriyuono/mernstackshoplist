d10a42bd513c26725cc09666063dec4ca22bc476
ibnutriyuono <ibnutriyuono@gmail.com>
2018-09-29 11:15:19 +0700

    give route for api

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
+
+const items = require('./routes/api/items');
 
 const app = express();
 
@@ -16,6 +19,8 @@ mongoose
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
+// Use Routes
+app.use('/api/items', items);
 
 const port = process.env.PORT || 5000;
 
